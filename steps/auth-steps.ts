b54ca43c1875982f95b212bdbea68a22c932a49e
QuocBao <quocbaovu15101996@gmail.com>
2025-11-12 14:08:42 +0700

    refactor: code

--- steps/auth-steps.ts
+++ steps/auth-steps.ts
@@ -1,5 +1,4 @@
-import { expect } from '@playwright/test';
-import { Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import LoginPage from '../pages/LoginPage';
 import ProductPage from '../pages/ProductPage';
 import { config } from '../utils/config';
@@ -26,10 +25,20 @@ export class AuthSteps {
   async loginWithInvalidCredentials(username: string, password: string) {
     await this.loginPage.navigateToLoginPage();
     await this.loginPage.login(username, password);
+    await this.verifyInvalidLoginError();
+  }
+
+  async verifyInvalidLoginError() {
+    expect(await this.loginPage.verifyErrorMessageIsVisible()).toBeTruthy();
+    const errorText = await this.loginPage.getErrorMessageText();
+    expect(errorText).toContain('Epic sadface: Username and password do not match any user in this service');
   }
 
   async verifyLoginIsSuccessful() {
     await expect(this.productPage.verifyPageIsDisplayed()).resolves.toBeTruthy();
+    await expect(this.page.getByText('Swag Labs')).toBeVisible();
+    await expect(this.page.locator('[data-test="inventory-container"]')).toBeVisible();
+    await expect(this.page.getByText('Products')).toBeVisible();
   }
 
   async verifyLoginButtonIsVisible() {
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -11,24 +11,15 @@ test.describe('Login Tests', () => {
     authSteps = new AuthSteps(page, loginPage);
   });
 
-  test('should login successfully with valid credentials and display products', async ({ page }) => {
+  test('@C02 Verify that the user can log in into saucedemo.com with valid credentials', async () => {
     await authSteps.loginWithValidCredentials();
-    
-    // Additional verifications
-    await expect(page.getByText('Swag Labs')).toBeVisible();
-    await expect(page.locator('[data-test="inventory-container"]')).toBeVisible();
-    await expect(page.getByText('Products')).toBeVisible();
   });
 
-  test('should display error message when logging in with wrong password', async () => {
+  test('@C01 Verify that the error message is displayed when use log in with invalid credentials', async () => {
     await authSteps.loginWithInvalidCredentials('standard_user', 'wrongpassword');
-    
-    expect(await loginPage.verifyErrorMessageIsVisible()).toBeTruthy();
-    const errorText = await loginPage.getErrorMessageText();
-    expect(errorText).toContain('Epic sadface: Username and password do not match any user in this service');
   });
 
-  test('should login and then logout successfully', async () => {
+  test('@C03 Verify that user can logout ', async () => {
     await authSteps.loginWithValidCredentials();
     await authSteps.logout();
   });
